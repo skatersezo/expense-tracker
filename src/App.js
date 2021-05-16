be44cf299d0677e6a54ac36274d1dfb38cd6483e
Ivan <ivansezoskate@gmail.com>
2021-05-16 19:35:50 +0900

    Refactors State in App.js

--- src/App.js
+++ src/App.js
@@ -3,41 +3,38 @@ import { useState } from 'react';
 import Expenses from './components/Expenses/Expenses';
 import NewExpense from './components/NewExpense/NewExpense';
 
+const DUMMY_EXPENSES = [
+  {
+    id: 'e1',
+    title: 'Toilet Paper',
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+  },
+  { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+  {
+    id: 'e3',
+    title: 'Car Insurance',
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: 'e4',
+    title: 'New Desk (Wooden)',
+    amount: 450,
+    date: new Date(2021, 5, 12),
+  },
+];
 
 function App() {
 
-  const [expenses, setExpenses] = useState({
-    items: [
-      {
-        id: 'e1',
-        title: 'Toilet Paper',
-        amount: 94.12,
-        date: new Date(2020, 7, 14),
-      },
-      { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
-      {
-        id: 'e3',
-        title: 'Car Insurance',
-        amount: 294.67,
-        date: new Date(2021, 2, 28),
-      },
-      {
-        id: 'e4',
-        title: 'New Desk (Wooden)',
-        amount: 450,
-        date: new Date(2021, 5, 12),
-      },
-    ]
-  });
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
 
 
   const addExpenseHandler = (newExpense) => {
-    setExpenses((prevState) => {
-      return {
-        items: prevState.items.concat(newExpense)
-      };
+    setExpenses((prevExpenses) => {
+      return [newExpense, ...prevExpenses];
     });
-  };
+  }
 
   const filterExpensesHandler = (filter) => {
     console.log(`Filter applied for year ${filter}`);
@@ -47,7 +44,7 @@ function App() {
     <div>
         <NewExpense onAddExpense={addExpenseHandler} />
         <Expenses 
-          items={expenses.items} 
+          items={expenses} 
           onFilter={filterExpensesHandler}
         />
     </div>
